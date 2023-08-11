3edb00c678af142770e263611da25dc3981ec0fc
Harlan George <hgeorge29@gmail.com>
2023-08-10 22:31:24 -0400

    createTables function finished

--- db/createTables.js
+++ db/createTables.js
@@ -12,20 +12,44 @@
         email varchar(255),
         first_name varchar(255),
         last_name varchar(255),
-        address_1,
-        address_2,
-        city varchar(255),
-        state varchar(255),
-        zip_code INTEGER
+        address_1 INTEGER,
+        address_2 INTEGER,
+        city varchar(255) NOT NULL,
+        state varchar(255) NOT NULL,
+        zip_code INTEGER,
+        country varchar(255) NOT NULL,
+        shipping_address_1  INTEGER,
+        shipping_address_2 varchar(255) NOT NULL,
+        shipping_city varchar(255) NOT NULL,
+        shipping_state varchar(255) NOT NULL,
+        shipping_zip_code INTEGER,
+        shipping_counry varchar(255) NOT NULL,
         );
       `);
 
       await client.query(`
       CREATE TABLE orders,
       id SERIAL PRIMARY KEY,
-      ORDER_item_id SERIAL PRIMARY KEY,
+      order_item_id SERIAL PRIMARY KEY,
       User_id SERIAL PRIMARY KEY,
-      email varchar(255)
+      billing_address_1 INTEGER NOT NULL,
+      billing_address_2 INTEGER NOT NULL,
+      billing_city TEXT NOT NULL,
+      billing_state TEXT NOT NULL,
+      billing_zip_code INTEGER NOT NULL,
+      billing_country,
+      email varchar(255),
+      shipping_address_1 varchar(255) NOT NULL,
+      shipping_address_2 varchar(255) NOT NULL,
+      shipping_city varchar(255) NOT NULL,
+      shipping_state varchar(255) NOT NULL,
+      shipping_zip_code INTEGER NOT NULL,
+      shipping_country varchar(255) NOT NULL,
+      email varchar(255) UNIQUE NOT NULL,
+      order_total INTEGER,
+      use_default_address BOOLEAN DEFAULT FALSE,
+      checkout_complete BOOLEAN DEFAULT FALSE,
+      order_fulfilled BOOLEAN DEFAULT FALSE
       );
       `);
 
@@ -42,24 +66,34 @@
       await client.query(`
       CREATE TABLE items,
       id SERIAL PRIMARY KEY, 
-      title varchar(255)
-      price INTEGER
-      inventory INTEGER
-      product_category varchar(255)
-      [image name] nvarchar(100)
+      title varchar(255),
+      price INTEGER,
+      inventory INTEGER,
+      product_category varchar(255),
+      [image name] nvarchar(100),
       [image] varbinary(max)
-      `)
-
-    // create table Item
+      );
+      `);
 
     // create table item_category
-
+      await client.query(`
+      CREATE TABLE item_category,
+      id SERIAL PRIMARY KEY,
+      product_id "INTEGER REFERENCES" item(id),
+      categoty_id SERIAL PRIMARY KEY
+      );
+      `)
     //create table category
+      await client.query(`
+      CREATE TABLE category,
+      id SERIAL PRIMARY KEY,
+      name varchar(255) UNIQUE NOT NULL
+      );
+      `)
 
     } catch (error) {
         console.error("Error creating tables")
         throw error;
     }
-async function 
 
  }
