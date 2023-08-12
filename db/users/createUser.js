cc9c0100679a0cff66544f7ff97e0cf25d6b4cb0
Craig Case <case2cn@gmail.com>
2023-08-12 15:34:46 -0600

    updating work

--- db/users/createUser.js
+++ db/users/createUser.js
@@ -6,6 +6,8 @@ import * as bcrypt from "bcrypt";
 
 async function createUser({ ...userFields }) {
   const SALT_COUNT = 10;
+  let data = Object.values(userFields)
+  console.log("FIELDS", userFields);
   const password = userFields.password;
 
   delete userFields.password; 
@@ -19,15 +21,19 @@ async function createUser({ ...userFields }) {
 
   columnNames = columnNames + `, password`
   let valuePlaceHolders = Object.keys(userFields)
-    .map((index) => `$${index + 1}`)
+    .map((fields) => {
+      for (let i=1; i <= fields.length; i++) {
+        return 
+      }
+    })
     .join(", ");
   
-  valuePlaceHolders = valuePlaceHolders + `${dataArray.length}`
+  valuePlaceHolders = valuePlaceHolders + `, $${dataArray.length +1}`
 
   const userSql = `INSERT INTO users (${columnNames}) VALUES (${valuePlaceHolders})
   ON CONFLICT (username) DO NOTHING RETURNING * ;`;
 
-  const data = [hashedPasssword];
+  data = [hashedPasssword];
   console.log(userSql)
   //const { rows: user } = await client.query(userSql, data);
   //delete user[0].password;
