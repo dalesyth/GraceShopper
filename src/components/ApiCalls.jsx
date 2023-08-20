d3f6b93023304423e0c217d6ef9202f0f3a82a53
Dale Syth <dalesyth@gmail.com>
2023-08-20 16:01:11 -0500

    created ItemDetail component

--- src/components/ApiCalls.jsx
+++ src/components/ApiCalls.jsx
@@ -18,3 +18,28 @@ export async function getAllItems() {
         console.error(error);
     }
 }
+
+// GET item by ID
+export async function getItemById(itemId) {
+    
+    console.log(`itemId from getItemById: ${itemId}`);
+    console.log(`API route from getItemById: ${APIURL}/items/${itemId}}`);
+    try {
+        const response = await fetch(`${APIURL}/items/${itemId}`, {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+
+        const result = await response.json();
+
+        return result;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+
+
+
--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { getItemById } from "./ApiCalls";
 
 const ItemDetail = () => {
-  return (
-    <div>ItemDetail</div>
-  )
-}
+  const { itemId } = useParams();
+  const [item, setItem] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [quantity, setQuantity] = useState(1);
 
-export default ItemDetail
+  useEffect(() => {
+    const getItemDetail = async () => {
+      try {
+        const response = await getItemById(itemId);
+        setItem(response);
+        setIsLoading(false);
+      } catch (error) {
+        console.error(error);
+        setIsLoading(false);
+      }
+    };
+    getItemDetail();
+  }, [itemId]);
+
+  const handleQuantity = (event) => {
+    setQuantity(event.target.value);
+  };
+
+  const handleAddToCart = async () => {
+    console.log(`You have reached handleAddToCart, quantity is ${quantity}`);
+  };
+
+  return (
+    <div className="flex h-screen items-center justify-center">
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : item ? (
+        <div className="flex flex-col h-screen items-center justify-center bg-gray-200 w-1/2 h-1/2 rounded-lg shadow-lg">
+          <h2 className="font-bold pb-2">{item.title}</h2>
+          <div>Price: {item.price}</div>
+          <div>
+            <label htmlFor="quantity">Quantity:</label>
+            <input
+              type="number"
+              id="quantity"
+              value={quantity}
+              onChange={handleQuantity}
+            ></input>
+          </div>
+          <div>
+            <button
+              className="bg-blue-400 text-white font-bold px-0.5 py-1 rounded-lg hover:bg-blue-600 hover:font-extrabold"
+              onClick={handleAddToCart}
+            >
+              Add To Cart
+            </button>
+          </div>
+        </div>
+      ) : (
+        <p>Item not found</p>
+      )}
+    </div>
+  );
+};
+
+export default ItemDetail;
