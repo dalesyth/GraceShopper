902485e7ba7dcf479a82dc504d183a3ed62a40f9
Craig Case <case2cn@gmail.com>
2023-08-24 20:13:36 -0600

    added conditional login/logout link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,18 @@
 import { Link } from "react-router-dom";
 
+
 const Navbar = () => {
+  
+  let loginLink = localStorage.getItem("token")? 
+      <Link to="/LogOut" className="no-underline p-6 hover:text-blue-600">
+        Logout
+      </Link>
+      :
+      <Link to="/login" className="no-underline p-6     hover:text-blue-600">
+        Login
+      </Link>;
+    
+  
   return (
     <nav className="navbar p-8 flex justify-between items-center max-w-screen-lg mx-auto border-b border-gray-200 bg-gray-200 shadow-lg">
       <h1 className="text-pink-600 text-xl font-bold">Graceshopper</h1>
@@ -11,13 +23,10 @@ const Navbar = () => {
         >
           Home
         </Link>
-        <Link to="/login" className="no-underline p-6 hover:text-blue-600">
-          Login
-        </Link>
-
         <Link to="/cart" className="no-underline p-6 hover:text-blue-600">
           Cart
         </Link>
+        {loginLink}
        
       </div>
     </nav>
