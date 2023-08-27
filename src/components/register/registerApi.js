d92558bc42a87da8e8cad448dd05c68397aa08a7
Harlan George <hgeorge29@gmail.com>
2023-08-27 17:08:08 -0400

    Updated register api in src

--- src/components/register/registerApi.js
+++ src/components/register/registerApi.js
@@ -1,37 +1,28 @@
 const APIURL = "http://localhost:3000/api";
 
-async function registerUser(username, password) {
-  console.log(`Inside registerUser from registerAPI.js, UserName: ${username}, password: ${password}`);
-  const fullAPIURL = `${APIURL}/users/register`;
-  const body = JSON.stringify({
-    username: username,
-    password: password,
-  });
+export async function registerUser(username, password) {
 
+  
+  console.log(`Inside registerUser from registerAPI.js, UserName: ${username}, password: ${password}`);
+  
   try {
-    const response = await fetch(fullAPIURL, {
+    const response = await fetch(`${APIURL}/users/register`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: body,
+      body: JSON.stringify({
+        user: {
+          username, 
+          password,
+        }
+      }),
     });
 
     const result = await response.json();
-    const token = result.token;
-
-    if (result.success) {
-    
-      localStorage.setItem("token", JSON.stringify(token));
-      localStorage.setItem("username", JSON.stringify(username));
-
-      return true;
-    } else {
-      return false;
-    }
+    return result.user;
   } catch (error) {
     console.error(`Register Error: ${error}`);
   }
 }
 
-export { registerUser }
