e62620f33ca766115fed64a372e65d7db7e84106
Harlan George <hgeorge29@gmail.com>
2023-08-30 20:45:24 -0400

    Updated registerApi.js

--- src/components/register/registerApi.js
+++ src/components/register/registerApi.js
@@ -3,18 +3,21 @@ const APIURL = "http://localhost:3000/api/users";
 export const registerUser = async (...fields) => {
   const username = fields[0].username;
   const password = fields[0].password;
+  console.log("Inside registerUser")
 
-console.log(fields[0].username)
-console.log(fields[0].password);
+console.log(username)
+console.log(password);
 
-  
   try {
+    
     const response = await fetch(`${APIURL}/register`, {
+     
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(fields[0]),
+      
     });
 
     const result = await response.json();
