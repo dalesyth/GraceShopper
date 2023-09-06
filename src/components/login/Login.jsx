446fe01ff3775dab6580290f20d9f1a5b8b020d0
Dale Syth <dalesyth@gmail.com>
2023-09-05 19:54:12 -0500

    build complete

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,25 +1,23 @@
-import { useState } from 'react';
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import { login } from "./loginHelpers"
+import { login } from "./loginHelpers";
 import { useNavigate } from "react-router";
 //import { LoggedIn } from "./LoggedIn";
 
-
-
 const Login = () => {
   const navigate = useNavigate();
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleUserChange = (event) => {
     setUsername(event.target.value);
     console.log(username);
-  }
+  };
 
   const handlePasswordChange = (event) => {
     setPassword(event.target.value);
     console.log(password);
-  }
+  };
 
   const handleSubmitButton = async (event) => {
     event.preventDefault();
@@ -31,17 +29,19 @@ const Login = () => {
         if (result) {
           //console.log(`STORED USERNAME: ${JSON.parse(localStorage.getItem("username"))}`);
           //console.log(`STORED TOKEN: ${JSON.parse(localStorage.getItem("token"))}`);
-          console.log(`STORED ROLE: ${JSON.parse(localStorage.getItem("role"))}`);
+          console.log(
+            `STORED ROLE: ${JSON.parse(localStorage.getItem("role"))}`
+          );
         } else {
           window.alert("Login Failed!");
           console.log("Login Failed!");
         }
       }
       navigate("/home");
-    } catch(error) {
-      console.error(`Login Error: ${error}`)
+    } catch (error) {
+      console.error(`Login Error: ${error}`);
     }
-  }
+  };
 
   if (!localStorage.getItem("token")) {
     return (
@@ -50,7 +50,9 @@ const Login = () => {
           <div className="flex justify-center items-center  py-24">
             <div className="w-6/12 max-w-[450px] h-100 rounded-lg mx-auto bg-gray-200 ">
               <div className="max-w-[320px] mx-auto py-6">
-                <label className="font-bold  text-5xl text-pink-600">Login</label>
+                <label className="font-bold  text-5xl text-pink-600">
+                  Login
+                </label>
                 <form className="w-full flex flex-col py-4">
                   <label className="text-xl font-bold">User Name</label>
                   <input
@@ -95,22 +97,21 @@ const Login = () => {
       </>
     );
   } else {
-  return (
-    <>
-      <div className="flex items-center justify-center ">
-        <div className="flex flex-col h-screen items-center justify-center bg-gray-200 w-8/12 h-24  rounded-lg shadow-lg">
-          <h2 className="font-bold pb-2 text-2xl">Already Logged In!</h2>
-          <div className="flex space-x-1 font-bold">
-            <Link to={`/logout`}>Log Out</Link>
-            <div>|</div>
-            <Link to={`/home`}>Cancel</Link>
+    return (
+      <>
+        <div className="flex items-center justify-center ">
+          <div className="flex flex-col h-screen items-center justify-center bg-gray-200 w-8/12 h-24  rounded-lg shadow-lg">
+            <h2 className="font-bold pb-2 text-2xl">Already Logged In!</h2>
+            <div className="flex space-x-1 font-bold">
+              <Link to={`/logout`}>Log Out</Link>
+              <div>|</div>
+              <Link to={`/home`}>Cancel</Link>
+            </div>
           </div>
         </div>
-      </div>
-    </>
-  ); 
+      </>
+    );
   }
-}
+};
 
-
-export default Login
+export default Login;
